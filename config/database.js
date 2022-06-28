7abe91473b728209f59aafa86ed84ccccc8ee287
Gabriele Napoli <gabriele.napoli@kalpa.it>
2022-06-28 14:37:10 +0200

    log db

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ const parse = require("pg-connection-string").parse;
 module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
     const config = parse(process.env.DATABASE_URL);
+    console.log("config", config);
     return {
       defaultConnection: "default",
       connections: {
